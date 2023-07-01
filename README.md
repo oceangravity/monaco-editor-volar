91d0bd10ec2e9f4c4eb9fc8e775d6b9c9304e881
oceangravity <elefanteblanco5@gmail.com>
2023-07-01 11:51:03 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# monaco-editor-volar
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-y2ggw6)
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div id="monaco-editor"></div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import * as monaco from 'monaco-editor-core';
+
+const value = `
+function hello() {
+  alert('Hello world!')
+}
+`;
+
+const globalContext = `let b = 10`;
+
+monaco.languages.typescript.typescriptDefaults.addExtraLib(globalContext);
+
+const editor = monaco.editor.create(document.getElementById('monaco-editor'), {
+  value,
+  language: 'typescript',
+  automaticLayout: true,
+});
+
+editor.setHiddenAreas([
+  new monaco.Range(
+    editor.getModel().getLineCount(),
+    0,
+    editor.getModel().getLineCount(),
+    0
+  ),
+  new monaco.Range(0, 0, 0, 0),
+]);
+</script>
